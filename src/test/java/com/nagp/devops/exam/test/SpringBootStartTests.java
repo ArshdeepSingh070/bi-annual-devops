caa52bb32510f698b2075a60e411bed7e6fa6710
Arshdeep Singh <arshdeep.singh@nagarro.com>
2021-08-15 17:56:24 +0530

    first-commit

---
+++ src/test/java/com/nagp/devops/exam/test/SpringBootStartTests.java
@@ -0,0 +1,16 @@
+package com.nagp.devops.exam.test;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootStartTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
